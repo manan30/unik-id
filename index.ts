cf9833c46c05f255d10b9c0a7323c54650d860f2
Manan Joshi <mananjoshi1995@gmail.com>
2022-07-31 23:38:51 -0500

    test

--- index.ts
+++ index.ts
@@ -2,5 +2,10 @@ export const unikId = () => {
 	const head = Date.now().toString(36);
 	const tail = Math.random().toString(36).substring(2);
 
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	// eslint-disable-next-line no-console
+	console.log(head + tail);
+
 	return head + tail;
 };
